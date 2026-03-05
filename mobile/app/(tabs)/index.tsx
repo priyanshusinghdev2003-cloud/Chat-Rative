9663b7cca14ff7db86fbce07bbca268a225abc4c
Priyanshu Singh <priyanshusinghdev2003@gmail.com>
2026-03-05 12:12:22 +0530

    feat: Initialize chat-native project with mobile application, backend service, and Docker configuration.

---
+++ mobile/app/(tabs)/index.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+export default function index() {
+  return (
+    <View>
+      <Text className="text-red-200">index</Text>
+    </View>
+  );
+}
--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,5 +1,35 @@
 import { Stack } from "expo-router";
+import "@/global.css";
+import { ClerkProvider } from "@clerk/clerk-expo";
+import { StatusBar } from "expo-status-bar";
+import { tokenCache } from "@clerk/clerk-expo/token-cache";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ClerkProvider tokenCache={tokenCache}>
+      <QueryClientProvider client={queryClient}>
+        <StatusBar style="light" />
+        <Stack
+          screenOptions={{
+            headerShown: false,
+            contentStyle: { backgroundColor: "#0D0D0F" },
+          }}
+        >
+          <Stack.Screen name="(auth)" options={{ animation: "fade" }} />
+          <Stack.Screen name="(tabs)" options={{ animation: "fade" }} />
+          <Stack.Screen
+            name="new-chat"
+            options={{
+              animation: "slide_from_bottom",
+              presentation: "modal",
+              gestureEnabled: true,
+            }}
+          />
+        </Stack>
+      </QueryClientProvider>
+    </ClerkProvider>
+  );
 }
