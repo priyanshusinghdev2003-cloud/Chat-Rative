8d5833e95535b08c16fc770027febefca398d2a0
Priyanshu Singh <priyanshusinghdev2003@gmail.com>
2026-03-07 11:25:46 +0530

    feat: Implement initial server setup, database connection, and socket initialization.

--- backend/index.ts
+++ backend/index.ts
@@ -1,5 +1,5 @@
-// import dns from "dns";
-// dns.setServers(["1.1.1.1", "8.8.8.8"]);
+import dns from "dns";
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 import app from "./src/app.js";
 import { createServer } from "http";
 import { connectDB } from "./src/config/database.js";
@@ -10,13 +10,8 @@ const httpServer = createServer(app);
 
 initializeSocket(httpServer);
 
-connectDB()
-  .then(() => {
-    httpServer.listen(PORT, () => {
-      console.log(`Server is running on PORT: http://localhost:${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error("Failed to start server:", error);
-    process.exit(1);
-  });
+await connectDB();
+
+httpServer.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
